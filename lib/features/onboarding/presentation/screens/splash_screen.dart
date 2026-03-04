6f71517b32d0ebab6209e2918032563e937ede9f
abibuali09 <abibuali09@gmail.com>
2026-03-04 11:16:36 +0000

    feat: Implement temporary role-based navigation to dedicated main wrappers on the splash screen and reformat animation code.

--- lib/features/onboarding/presentation/screens/splash_screen.dart
+++ lib/features/onboarding/presentation/screens/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'package:car225/features/agent/presentation/screens/agent_main_wrapper.dart';
+import 'package:car225/features/driver/presentation/screens/driver_main_wrapper.dart';
+import 'package:car225/features/hostess/presentation/screens/hostess_main_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -52,9 +55,18 @@ class _SplashScreenState extends State<SplashScreen> {
     if (!mounted) return;
 
     // CHOIX DE LA PAGE DE DESTINATION
+    // TEMPORAIRE : On redirige vers l'écran Driver pour le développement
+    const String userType = "hostess";
+
     final Widget destination = _isConnected
-        ? const MainScreen()       // Si connecté -> Dashboard
-        : const OnboardingScreen(); // Si pas connecté -> Onboarding
+        ? const MainScreen()
+        : userType == "hostess"
+        ? const HostessMainWrapper()
+        : userType == "driver"
+        ? const DriverMainWrapper()
+        : userType == "agent"
+        ? const AgentMainWrapper()
+        : const OnboardingScreen();
 
     Navigator.of(context).pushReplacement(
       PageRouteBuilder(
@@ -63,7 +75,9 @@ class _SplashScreenState extends State<SplashScreen> {
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           const curve = Curves.easeInOut;
           return FadeTransition(
-            opacity: animation.drive(Tween(begin: 0.0, end: 1.0).chain(CurveTween(curve: curve))),
+            opacity: animation.drive(
+              Tween(begin: 0.0, end: 1.0).chain(CurveTween(curve: curve)),
+            ),
             child: child,
           );
         },
@@ -97,50 +111,121 @@ class _SplashScreenState extends State<SplashScreen> {
             children: [
               // --- 0. L'ANNEAU FIN ---
               Center(
-                child: Container(
-                  width: 115, height: 115,
-                  decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.yellowAccent.withOpacity(0.7), width: 2)),
-                ).animate()
-                    .scale(duration: 1000.ms, begin: const Offset(1, 1), end: const Offset(1.15, 1.15), curve: Curves.easeInOut)
-                    .tint(color: Colors.white, duration: 1000.ms)
-                    .then(delay: 500.ms).fadeOut(duration: 300.ms),
+                child:
+                    Container(
+                          width: 115,
+                          height: 115,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.yellowAccent.withOpacity(0.7),
+                              width: 2,
+                            ),
+                          ),
+                        )
+                        .animate()
+                        .scale(
+                          duration: 1000.ms,
+                          begin: const Offset(1, 1),
+                          end: const Offset(1.15, 1.15),
+                          curve: Curves.easeInOut,
+                        )
+                        .tint(color: Colors.white, duration: 1000.ms)
+                        .then(delay: 500.ms)
+                        .fadeOut(duration: 300.ms),
               ),
 
               // --- 1. LE CERCLE BLANC ---
               Center(
-                child: Container(
-                  width: 100, height: 100,
-                  decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-                ).animate()
-                    .scale(duration: 1000.ms, begin: const Offset(1, 1), end: const Offset(1.1, 1.1), curve: Curves.easeInOut)
-                    .then().scale(delay: 500.ms, duration: 800.ms, end: const Offset(30, 30), curve: Curves.easeInExpo),
+                child:
+                    Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                        )
+                        .animate()
+                        .scale(
+                          duration: 1000.ms,
+                          begin: const Offset(1, 1),
+                          end: const Offset(1.1, 1.1),
+                          curve: Curves.easeInOut,
+                        )
+                        .then()
+                        .scale(
+                          delay: 500.ms,
+                          duration: 800.ms,
+                          end: const Offset(30, 30),
+                          curve: Curves.easeInExpo,
+                        ),
               ),
 
               // --- 2. LE LOGO "A" ---
               Center(
                 child: SvgPicture.asset('assets/vectors/logo_A.svg', height: 60)
-                    .animate().fadeIn(duration: 500.ms).then(delay: 1000.ms).fadeOut(duration: 300.ms, curve: Curves.easeOut),
+                    .animate()
+                    .fadeIn(duration: 500.ms)
+                    .then(delay: 1000.ms)
+                    .fadeOut(duration: 300.ms, curve: Curves.easeOut),
               ),
 
               // --- 3. LA PARENTHÈSE / ARCHE (Partie HAUTE) ---
               Positioned(
-                top: size.height * 0.50 - 35, left: 0, right: 0,
-                child: Center(child: SvgPicture.asset('assets/vectors/logo_text_top.svg', height: 30)),
-              ).animate(delay: startTextDelay.ms)
-                  .moveY(begin: -250, end: 0, duration: moveDuration.ms, curve: Curves.easeOutQuart)
+                    top: size.height * 0.50 - 35,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'assets/vectors/logo_text_top.svg',
+                        height: 30,
+                      ),
+                    ),
+                  )
+                  .animate(delay: startTextDelay.ms)
+                  .moveY(
+                    begin: -250,
+                    end: 0,
+                    duration: moveDuration.ms,
+                    curve: Curves.easeOutQuart,
+                  )
                   .fadeIn(duration: 200.ms)
                   .then(delay: pauseAuCentre.ms)
-                  .moveY(begin: 0, end: ascensionDistance, duration: ascensionDuration.ms, curve: Curves.easeInOutCubic),
+                  .moveY(
+                    begin: 0,
+                    end: ascensionDistance,
+                    duration: ascensionDuration.ms,
+                    curve: Curves.easeInOutCubic,
+                  ),
 
               // --- 4. LE TEXTE "CAR 225" (Partie BASSE) ---
               Positioned(
-                top: size.height * 0.50 - 5, left: 0, right: 0,
-                child: Center(child: SvgPicture.asset('assets/vectors/logo_text_bottom.svg', height: 70)),
-              ).animate(delay: startTextDelay.ms)
-                  .moveY(begin: 250, end: 0, duration: moveDuration.ms, curve: Curves.easeOutQuart)
+                    top: size.height * 0.50 - 5,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'assets/vectors/logo_text_bottom.svg',
+                        height: 70,
+                      ),
+                    ),
+                  )
+                  .animate(delay: startTextDelay.ms)
+                  .moveY(
+                    begin: 250,
+                    end: 0,
+                    duration: moveDuration.ms,
+                    curve: Curves.easeOutQuart,
+                  )
                   .fadeIn(duration: 200.ms)
                   .then(delay: pauseAuCentre.ms)
-                  .moveY(begin: 0, end: ascensionDistance, duration: ascensionDuration.ms, curve: Curves.easeInOutCubic),
+                  .moveY(
+                    begin: 0,
+                    end: ascensionDistance,
+                    duration: ascensionDuration.ms,
+                    curve: Curves.easeInOutCubic,
+                  ),
             ],
           ),
         ),
